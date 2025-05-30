4faf250aef5d70252ec6427595108c8c33105e12
AbdulRahman_Salah <117433975+AbdulRahmanSalaah@users.noreply.github.com>
2025-05-31 00:47:28 +0300

    Time: 33 ms (79.87%), Space: 81 MB (87.16%) - LeetHub

---
+++ 0883-car-fleet/0883-car-fleet.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int carFleet(int target, vector<int> &position, vector<int> &speed)
+    {
+
+        int n = position.size();
+        vector<pair<int, double>> cars(n);
+        for (int i = 0; i < n; ++i)
+        {
+            int dist = target - position[i];
+            double time = (double)dist / speed[i];
+            cars[i] = {position[i], time};
+        }
+        stack<double> st;
+        sort(cars.begin(), cars.end(), greater<pair<int, double>>());
+        for (int i = 0; i < n; ++i)
+        {
+            double time = cars[i].second;
+            if (st.empty() || st.top() < time)
+            {
+                st.push(time);
+            }
+        }
+        return st.size();
+    }
+};
