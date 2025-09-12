87fd91376390e1c8b81ffd772cbe656881c063dc
AbdulRahman_Salah <117433975+AbdulRahmanSalaah@users.noreply.github.com>
2025-09-12 08:53:40 +0300

    Time: 3 ms (35.6%), Space: 112.3 MB (95.16%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -14,8 +14,7 @@ public:
         int tank = 0;
         int start = 0;
         for (int i = 0; i < gas.size(); i++) {
-            tank += gas[i];
-            tank -= cost[i];
+            tank += (gas[i] - cost[i]);
             if (tank < 0) {
                 tank = 0;
                 start = i + 1;
