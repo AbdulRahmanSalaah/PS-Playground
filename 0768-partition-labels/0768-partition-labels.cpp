a4b6c0e188be83a2a71089da0748f539023b3353
AbdulRahman_Salah <117433975+AbdulRahmanSalaah@users.noreply.github.com>
2025-09-14 06:31:14 +0300

    Time: 3 ms (28.88%), Space: 8.9 MB (69.92%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.cpp
+++ 0768-partition-labels/0768-partition-labels.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     vector<int> partitionLabels(string s) {
         vector<int> ans;
-        vector<int> last(26, 0);
+        unordered_map<char, int> last;
 
         for (int i = 0; i < s.size(); i++) {
-            last[s[i] - 'a'] = i;
+            last[s[i]] = i;
         }
 
         int start = 0, end = 0;
         for (int i = 0; i < s.size(); i++) {
-            end = max(end, last[s[i] - 'a']);
+            end = max(end, last[s[i]]);
             if (i == end) {
                 ans.push_back(end - start + 1);
                 start = i + 1;
