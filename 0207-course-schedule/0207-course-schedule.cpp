cf8cca38c4f9b3e631e596b5de5572b1803ace5c
AbdulRahman_Salah <117433975+AbdulRahmanSalaah@users.noreply.github.com>
2025-09-20 18:51:49 +0300

    Time: 8 ms (36.94%), Space: 19.4 MB (58.18%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    bool iscycle(vector<int> adj[], vector<int>& vis, int id) {
+        if (vis[id] == 1)
+            return true;
+        if (vis[id] == 0) {
+            vis[id] = 1;
+            for (auto edge : adj[id]) {
+                if (iscycle(adj, vis, edge))
+                    return true;
+            }
+        }
+        vis[id] = 2;
+        return false;
+    }
+
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> adj[numCourses];
+        vector<int> vis(numCourses, 0);
+
+        for (auto edge : prerequisites) {
+            adj[edge[1]].push_back(edge[0]);
+        }
+        for (int i = 0; i < numCourses; i++) {
+            if (iscycle(adj, vis, i))
+                return false;
+        }
+        return true;
+    }
+};
