efebfb9e104b2bd2fabcf7335e4760313d21e7fa
AbdulRahman_Salah <117433975+AbdulRahmanSalaah@users.noreply.github.com>
2025-10-10 16:18:52 +0300

    Time: 64 ms (61.6%), Space: 14.4 MB (63.37%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,27 +1,15 @@
 class Solution {
-    vector<int> memo;
-
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        memo.assign(n, -1);
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            ans = max(ans, dfs(i, nums));
+        vector<int> LIS(nums.size(), 1);
+
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[i] < nums[j]) {
+                    LIS[i] = max(LIS[i], 1 + LIS[j]);
+                }
+            }
         }
-        return ans;
-    }
-
-    int dfs(int i, vector<int>& nums) {
-        if (memo[i] != -1)
-            return memo[i];
-
-        int best = 1;
-        for (int j = i + 1; j < nums.size(); j++) {
-            if (nums[j] > nums[i])
-                best = max(best, 1 + dfs(j, nums));
-        }
-
-        return memo[i] = best;
+        return *max_element(LIS.begin(), LIS.end());
     }
 };
