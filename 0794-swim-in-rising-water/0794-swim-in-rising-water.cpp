33c139a2e08b93d0d98229d9086b88c5ef7f1e7d
AbdulRahman_Salah <117433975+AbdulRahmanSalaah@users.noreply.github.com>
2025-10-21 20:37:37 +0300

    Time: 0 ms (100%), Space: 13.1 MB (73.98%) - LeetHub

---
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int swimInWater(vector<vector<int>>& grid) {
+        int n = grid.size();
+        vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
+        vector<vector<bool>> vis(n, vector<bool>(n, false));
+        
+        int dx[4] = {0, 1, 0, -1};
+        int dy[4] = {1, 0, -1, 0};
+
+        priority_queue<
+            pair<int, pair<int, int>>,
+            vector<pair<int, pair<int, int>>>,
+            greater<pair<int, pair<int, int>>>
+        > pq;
+
+        dist[0][0] = grid[0][0];
+        pq.push({grid[0][0], {0, 0}});
+
+        while (!pq.empty()) {
+            auto [time, cell] = pq.top();
+            pq.pop();
+            int x = cell.first, y = cell.second;
+
+            if (vis[x][y]) continue;
+            vis[x][y] = true;
+
+            if (x == n - 1 && y == n - 1) return time;
+
+            for (int dir = 0; dir < 4; dir++) {
+                int nx = x + dx[dir];
+                int ny = y + dy[dir];
+
+                if (nx >= 0 && ny >= 0 && nx < n && ny < n && !vis[nx][ny]) {
+                    int newTime = max(time, grid[nx][ny]);
+                    if (newTime < dist[nx][ny]) {
+                        dist[nx][ny] = newTime;
+                        pq.push({newTime, {nx, ny}});
+                    }
+                }
+            }
+        }
+
+        return -1; 
+    }
+};
