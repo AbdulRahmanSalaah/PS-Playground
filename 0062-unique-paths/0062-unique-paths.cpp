cf0ccd7fde7af05c5403a0d8d0d7018a98eeaea2
AbdulRahman_Salah <117433975+AbdulRahmanSalaah@users.noreply.github.com>
2025-10-31 07:56:28 +0200

    Time: 0 ms (100%), Space: 9.3 MB (40.57%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,26 @@
+class Solution {
+
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return dfs(0, 0, m, n, dp);
+    }
+
+    int dfs(int i, int j, int m, int n, vector<vector<int>>& dp) {
+        if (i >= m || j >= n)
+            return 0;
+
+        if (i == m - 1 && j == n - 1)
+            return 1;
+
+
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        int r = dfs(i, j + 1, m, n, dp);
+        int d = dfs(i + 1, j, m, n, dp);
+
+        return dp[i][j] = r + d;
+    }
+};
