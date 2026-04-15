58d116a3d7a9269a3e66accaa3012947ddf88be6
AbdulRahman_Salah <117433975+AbdulRahmanSalaah@users.noreply.github.com>
2026-04-15 05:26:07 +0200

    Time: 0 ms (100%), Space: 13 MB (72.28%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,20 +1,13 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        mp = {}
-        n = len(nums)
-
-        for i in range(n):
-            mp[nums[i]]=i
-
-        for i in range (n):
-            c = target - nums[i]
-            if( c in mp and mp[c]!=i ):
-                return [i, mp[c]]
-
-        return []
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+        mp = {}  
         
+        for i, num in enumerate(nums):
+            c = target - num
+            
+            if c in mp:
+                return [mp[c], i]
+            
+            mp[num] = i
+            
+        return []
