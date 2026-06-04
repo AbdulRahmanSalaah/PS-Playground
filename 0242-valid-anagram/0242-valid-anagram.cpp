0dec5395e54c513b6c4b1089f7350087e8e2af38
AbdulRahman_Salah <117433975+AbdulRahmanSalaah@users.noreply.github.com>
2026-06-04 07:11:35 +0300

    Time: 7 ms (19.16%), Space: 9.9 MB (25.02%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map<char, int> mp;
+        for (auto ch : s) {
+            mp[ch]++;
+        }
+        for (auto ch : t) {
+            mp[ch]--;
+        }
+        for (auto it : mp) {
+            if (it.second != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
