ccfb25a271a946dfce332f32b259603cd65ab6fd
AbdulRahman_Salah <117433975+AbdulRahmanSalaah@users.noreply.github.com>
2026-06-20 18:21:42 +0300

    Time: 23 ms (5.48%), Space: 22.9 MB (46.24%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,29 +1,20 @@
-#include <bits/stdc++.h>
-using namespace std;
-class Solution
-{
+class Solution {
 public:
-    int minEatingSpeed(vector<int> &piles, int h)
-    {
+    int minEatingSpeed(vector<int>& piles, int h) {
         int l = 1;
         int r = *max_element(piles.begin(), piles.end());
+
         int mink = r;
-        while (l <= r)
-        {
+        while (l <= r) {
             int k = (l + r) / 2;
             long long tottime = 0;
-
-            for (int i = 0; i < piles.size(); i++)
-            {
+            for (int i = 0; i < piles.size(); i++) {
                 tottime += ceil((double)piles[i] / k);
             }
-            if (tottime <= h)
-            {
+            if (tottime <= h) {
                 mink = k;
                 r = k - 1;
-            }
-            else
-            {
+            } else {
                 l = k + 1;
             }
         }
