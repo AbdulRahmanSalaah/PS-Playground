c024d9952077356ab9e7ccd4152149ca03e8b71c
AbdulRahman_Salah <117433975+AbdulRahmanSalaah@users.noreply.github.com>
2026-06-29 09:37:16 +0300

    Time: 8 ms (38.61%), Space: 11 MB (19.18%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,5 +1,3 @@
-#include <bits/stdc++.h>
-using namespace std;
 class Solution {
 public:
     int characterReplacement(string s, int k) {
@@ -7,11 +5,9 @@ public:
         int l = 0, r = 0;
         int res = 0;
         int maxFreq = 0;
-
         while (r < s.size()) {
             mp[s[r]]++;
             maxFreq = max(maxFreq, mp[s[r]]);
-
             while (r - l + 1 - maxFreq > k) {
                 mp[s[l]]--;
                 l++;
@@ -20,7 +16,6 @@ public:
             res = max(res, r - l + 1);
             r++;
         }
-        return res;
-        
+         return res;
     }
 };
